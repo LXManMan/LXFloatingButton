e95cf4c1e5c1694874ef3715350f2b1bffc028e9
雪晟 <missli_211@163.com>
2018-04-12 14:54:31 +0800

    Initial Commit

---
+++ LXFloatingButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXFloatingButton
+//
+//  Created by 漫漫 on 2018/4/12.
+//  Copyright © 2018年 漫漫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXFloatingButton/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LXFloatingButton
+//
+//  Created by 漫漫 on 2018/4/12.
+//  Copyright © 2018年 漫漫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
